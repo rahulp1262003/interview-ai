ed4f065f7eaf8862c68be5ea3fd268fc1cd6161c
rahulp1262003 <prajapatirahul1262003@gmail.com>
2026-05-12 12:01:48 +0530

    fix: update API base URL to use environment variable for better configuration

--- Frontend/src/features/interview/services/interview.ai.js
+++ Frontend/src/features/interview/services/interview.ai.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const authApi = axios.create({
-    baseURL: "http://localhost:4000",
+     baseURL: `${import.meta.env.VITE_API_URL}`,
     withCredentials: true,
 });
 
